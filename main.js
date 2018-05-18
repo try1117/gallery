8bccc1dd5d7cf9875c7755ba25230b2ee004df32
danilov <danilov.roman97@gmail.com>
2018-05-18 23:34:24 +1000

    Add images paths

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 let defaultImageURL = "http://seismonepal.gov.np/assets/img/no_image.jpg";
 
+// let imagesURL = [
+//     "https://upload.wikimedia.org/wikipedia/commons/9/93/20030820-antelope-canyon.jpg",
+//     "https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_960_720.jpg",
+//     "https://images.pexels.com/photos/34950/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/34923/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/34913/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/34942/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/34953/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/34922/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/34930/pexels-photo.jpg",
+//     "https://images.pexels.com/photos/349/pexels-photo.jpg",
+// ];
+
 let imagesURL = [
-    "https://upload.wikimedia.org/wikipedia/commons/9/93/20030820-antelope-canyon.jpg",
-    "https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_960_720.jpg",
-    "https://images.pexels.com/photos/34950/pexels-photo.jpg",
-    "https://images.pexels.com/photos/34923/pexels-photo.jpg",
-    "https://images.pexels.com/photos/34913/pexels-photo.jpg",
-    "https://images.pexels.com/photos/34942/pexels-photo.jpg",
-    "https://images.pexels.com/photos/34953/pexels-photo.jpg",
-    "https://images.pexels.com/photos/34922/pexels-photo.jpg",
-    "https://images.pexels.com/photos/34930/pexels-photo.jpg",
-    "https://images.pexels.com/photos/349/pexels-photo.jpg",
+    "images/0.jpg",
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+    "images/10.jpg",
 ];
 
 window.onload = function() {
